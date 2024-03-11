6746328f18f3fec48615904878345ed675eb473d
SK <kononenkoss@yandex.ru>
2024-03-11 21:49:46 +0300

    hw1_test11

--- src/doublearray.js
+++ src/doublearray.js
@@ -2,6 +2,3 @@ function doubleArrayElements(arr) {
     return arr.map(item => item * 2);
   }
 
-const originalArray = [1, 2, 3, 4, 5];
-const newArray = doubleArrayElements(originalArray);
-console.log(newArray); // Output: [2, 4, 6, 8, 10] 
--- src/doublearray.test.js
+++ src/doublearray.test.js
@@ -1,6 +1,6 @@
 const doubleArrayElements = require('./doublearray');
 test("Double Array", function() {    
-    expect(mostNumber(1, 2, 3, 4, 5)).toBe(2, 4, 6, 8, 10);
+    expect(doubleArrayElements([1, 2, 3, 4, 5])).toEqual([2, 4, 6, 8, 10]);
 });
  
 
