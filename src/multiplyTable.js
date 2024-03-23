2adfde149044fbb826f8ce0baaf7545e333e3301
SK <kononenkoss@yandex.ru>
2024-03-23 14:05:44 +0300

    доп тест

---
+++ src/multiplyTable.js
@@ -0,0 +1,4 @@
+function multiplyTable(number) {
+    return number * 7;
+}
+module.exports = multiplyTable;
---
+++ src/multiplyTable.test.js
@@ -0,0 +1,5 @@
+const multiplyTable = require('./multiplyTable');
+test("multiplyTable", function() {    
+    expect(multiplyTable(3)).toBe(21);
+    // expect(multiplyTable(5)).toBe(35);
+}); 
