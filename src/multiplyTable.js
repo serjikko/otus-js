e2d929b2aeb67fbf6a1120463535e4e752697ec6
SK <kononenkoss@yandex.ru>
2024-04-22 20:26:38 +0300

    na7

--- src/multiplyTable.js
+++ src/multiplyTable.js
@@ -1,4 +1,9 @@
-function multiplyTable(number) {
-    return number * 7;
+for (let i = 1; i <= 9; i++) {
+    function multiplyTable(number) {
+        const result = number * 7;
+        console.log(number + ' X 7 = ' + result);
+        return result;
+    }
 }
+
 module.exports = multiplyTable;
