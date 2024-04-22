4cb8fc2b14df21a9d50c29ea077ec3a0d0a5701d
SK <kononenkoss@yandex.ru>
2024-04-22 22:42:03 +0300

    рефак

--- src/word300.js
+++ src/word300.js
@@ -1,4 +1,5 @@
 function word300(number) {
-    return String(number).split('').map(Number);
+    return Math.floor(number / 100) + Math.floor((number % 100) / 10) + number % 10;
+
 }
 module.exports = word300;
--- src/word300.test.js
+++ src/word300.test.js
@@ -1,5 +1,5 @@
 const word300 = require('./word300');
 
 test("word300", function() {    
-    expect(word300(331)).toEqual([3,3,1]);
+    expect(word300(331)).toEqual(7);
 });
